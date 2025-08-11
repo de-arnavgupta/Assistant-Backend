02fb2aa9ac2ca6c86744aa7b27a1420f54929bf9
de-arnavgupta <arnavgpt28@outlook.com>
2025-08-11 23:36:16 +0530

    initialized the project

---
+++ src/main/java/com/arnavgpt/assistantbackend/AssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arnavgpt.assistantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arnavgpt/assistantbackend/AssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnavgpt.assistantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
